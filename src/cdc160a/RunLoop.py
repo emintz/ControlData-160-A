49b78c2072c4b297b2d8b9b4d139e0cafdaf314a
Eric Mintz <16582299+emintz@users.noreply.github.com>
2024-12-11 16:00:57 -0500

    Clean up and document the run loop.

--- src/cdc160a/RunLoop.py
+++ src/cdc160a/RunLoop.py
@@ -24,12 +24,16 @@ class RunLoop:
                testing under PyUnit, a character-mode console for
                development, or a full-blown GUI.
         :param storage: CDC 160-A memory and register file
-
         """
         self.__console = console
         self.__storage = storage
 
     def single_step(self) -> bool:
+        """
+        Run a single instruction.
+
+        :return: True unless an ERR or HLT instruction is run.
+        """
         self.__console.before_instruction_fetch(self.__storage)
         self.__storage.service_pending_interrupts()
         # TODO(emintz): service pending buffer requests
@@ -39,27 +43,17 @@ class RunLoop:
         current_instruction.determine_effective_address(self.__storage)
         self.__console.before_instruction_logic(self.__storage)
         current_instruction.perform_logic(self.__storage)
-        # result = self.__console.before_advance(self.__storage)
-        # if not result:
-        #     self.__storage.advance_to_next_instruction()
         if not self.__console.before_advance(self.__storage):
             return False
         self.__storage.advance_to_next_instruction()
         return True
 
     def run(self) -> None:
+        """
+        Runs the emulator, returning when the emulator performs an
+        ERR or HLT instruction.
+
+        :return: None
+        """
         while self.single_step():
             pass
-        # while True:
-        #     self.__console.before_instruction_fetch(self.__storage)
-        #     # TODO(emintz): respond to pending interrupts
-        #     # TODO(emintz): service pending buffer requests
-        #     self.__storage.unpack_instruction()
-        #     decoder = InstructionDecoder.decoder_at(self.__storage.f_instruction)
-        #     current_instruction = decoder.decode(self.__storage.f_e)
-        #     current_instruction.determine_effective_address(self.__storage)
-        #     self.__console.before_instruction_logic(self.__storage)
-        #     current_instruction.perform_logic(self.__storage)
-        #     if not self.__console.before_advance(self.__storage):
-        #         break
-        #     self.__storage.advance_to_next_instruction()
