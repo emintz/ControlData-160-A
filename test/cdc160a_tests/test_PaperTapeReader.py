3d6235a7781fdf458531cf61f3a11bc665ccd8b4
Eric Mintz <16582299+emintz@users.noreply.github.com>
2024-12-28 12:27:16 -0500

    Refactor to create temporary files as context managers. Need to do the same for reading them.

--- test/cdc160a_tests/test_PaperTapeReader.py
+++ test/cdc160a_tests/test_PaperTapeReader.py
@@ -10,6 +10,14 @@ class TestPaperTapeReader(TestCase):
     def setUp(self) -> None:
         self.__paper_tape_reader = PaperTapeReader()
 
+    @staticmethod
+    def _create_temp_file(contents: str) -> str:
+        with NamedTemporaryFile("w+", delete=False) as temp_file:
+            file_name = temp_file.name
+            print("Temporary file: {0},".format(temp_file.name))
+            temp_file.write(contents)
+        return file_name
+
     def test_accepts(self) -> None:
         assert self.__paper_tape_reader.accepts(0o4102)
         assert  not self.__paper_tape_reader.accepts(0o4101)
@@ -33,15 +41,12 @@ class TestPaperTapeReader(TestCase):
         assert io_channel_support == IOChannelSupport.NORMAL_ONLY
 
     def test_open_invalid_input_close(self) -> None:
-        temp_file = NamedTemporaryFile("w+", delete=False)
-        print("Temporary file: {0},".format(temp_file.name))
-        temp_file.write("0\n7\n007\ngorp\n456\n")
-        temp_file.close()
+        temp_file_name = self._create_temp_file("0\n7\n007\ngorp\n456\n")
         assert not self.__paper_tape_reader.is_open()
-        self.__paper_tape_reader.open(temp_file.name)
+        self.__paper_tape_reader.open(temp_file_name)
         assert self.__paper_tape_reader.is_open()
         assert (self.__paper_tape_reader.name() ==
-                temp_file.name)
+                temp_file_name)
         read_status, data = self.__paper_tape_reader.read()
         assert read_status
         assert data == 0
@@ -59,19 +64,15 @@ class TestPaperTapeReader(TestCase):
         assert data == 0o456
         self.__paper_tape_reader.close()
         assert not self.__paper_tape_reader.is_open()
-        os.unlink(temp_file.name)
-        assert not os.path.exists(temp_file.name)
+        os.unlink(temp_file_name)
+        assert not os.path.exists(temp_file_name)
 
     def test_open_valid_input_close(self) -> None:
-        temp_file = NamedTemporaryFile("w+", delete=False)
-        print("Temporary file: {0},".format(temp_file.name))
-        temp_file.write("0\n7\n007\n456\n")
-        temp_file.close()
+        temp_file_name = self._create_temp_file("0\n7\n007\n456\n")
         assert not self.__paper_tape_reader.is_open()
-        self.__paper_tape_reader.open(temp_file.name)
+        self.__paper_tape_reader.open(temp_file_name)
         assert self.__paper_tape_reader.is_open()
-        assert (self.__paper_tape_reader.name() ==
-                temp_file.name)
+        assert self.__paper_tape_reader.name() == temp_file_name
         read_status, data = self.__paper_tape_reader.read()
         assert read_status
         assert data == 0
@@ -86,20 +87,17 @@ class TestPaperTapeReader(TestCase):
         assert data == 0o456
         self.__paper_tape_reader.close()
         assert not self.__paper_tape_reader.is_open()
-        os.unlink(temp_file.name)
-        assert not os.path.exists(temp_file.name)
+        os.unlink(temp_file_name)
+        assert not os.path.exists(temp_file_name)
 
     def test_read_delay(self) -> None:
         assert self.__paper_tape_reader.read_delay() == 446
 
     def test_valid_external_function_tape_mounted(self) -> None:
-        temp_file = NamedTemporaryFile("w+", delete=False)
-        print("Temporary file: {0},".format(temp_file.name))
-        temp_file.write("0\n7\n007\n456\n")
-        temp_file.close()
+        temp_file_name = self._create_temp_file("0\n7\n007\n456\n")
 
         assert not self.__paper_tape_reader.is_open()
-        self.__paper_tape_reader.open(temp_file.name)
+        self.__paper_tape_reader.open(temp_file_name)
 
         (valid_request, status) = self.__paper_tape_reader.external_function(
             0o4102)
@@ -108,8 +106,8 @@ class TestPaperTapeReader(TestCase):
 
         self.__paper_tape_reader.close()
         assert not self.__paper_tape_reader.is_open()
-        os.unlink(temp_file.name)
-        assert not os.path.exists(temp_file.name)
+        os.unlink(temp_file_name)
+        assert not os.path.exists(temp_file_name)
 
     def test_valid_external_function_no_tape_mounted(self) -> None:
         (valid_request, status) = self.__paper_tape_reader.external_function(
@@ -118,13 +116,10 @@ class TestPaperTapeReader(TestCase):
         assert status is None
 
     def test_invalid__external_function_tape_mounted(self) -> None:
-        temp_file = NamedTemporaryFile("w+", delete=False)
-        print("Temporary file: {0},".format(temp_file.name))
-        temp_file.write("0\n7\n007\n456\n")
-        temp_file.close()
+        temp_file_name = self._create_temp_file("0\n7\n007\n456\n")
 
         assert not self.__paper_tape_reader.is_open()
-        self.__paper_tape_reader.open(temp_file.name)
+        self.__paper_tape_reader.open(temp_file_name)
 
         (valid_request, status) = self.__paper_tape_reader.external_function(
             0o4100)
@@ -133,5 +128,5 @@ class TestPaperTapeReader(TestCase):
 
         self.__paper_tape_reader.close()
         assert not self.__paper_tape_reader.is_open()
-        os.unlink(temp_file.name)
-        assert not os.path.exists(temp_file.name)
+        os.unlink(temp_file_name)
+        assert not os.path.exists(temp_file_name)
