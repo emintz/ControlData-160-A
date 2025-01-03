253b9e2370589c44aa836e57c6b8863b9259c393
BOB ROSWELL <broswell@syssrc.com>
2025-01-03 14:28:58 -0500

    Implement a boot command in the character-mode console.

--- src/system_specific/Factory.py
+++ src/system_specific/Factory.py
@@ -1,6 +1,8 @@
 import sys
 from typing import Callable
 from system_specific.linux import Linux
+from system_specific.windows import Windows
+
 
 def vacuous() -> bool:
     return True
@@ -9,6 +11,8 @@ def check_console_for_input() -> Callable[[], bool]:
     match sys.platform:
         case "linux":
             return Linux.check_for_input
+        case "win32":
+            return Windows.check_for_input_win
         case _:
             exception = NotImplemented()
             exception.add_note(
